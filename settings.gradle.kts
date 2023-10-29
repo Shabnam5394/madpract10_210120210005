ab776888e96d7ec9de84f44661f583e4f73212f5
Ansari Shabnam <ansarimuskan5394@gmail.com>
2023-10-29 10:46:55 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "madpract10_210120210005"
+include(":app")
